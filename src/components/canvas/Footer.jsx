baa6dc01480f116090a5c1e0ab55f4bd4dbafde5
123-fa <orafufrancis22@gmail.com>
2023-06-08 03:20:37 -0700

    first commit

---
+++ src/components/canvas/Footer.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+// import React from 'react';
+// function Footer () {
+//     return <div style={{textAlign: center}}>
+//         copyright frantech.
+//     </div>
+// }
+
+// export default Footer
